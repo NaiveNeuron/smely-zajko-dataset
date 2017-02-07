32d3a352505f6c3c8bfa56603223b085475e41db
Ondrej Jariabka <o.jariabka@gmail.com>
2017-02-07 11:34:05 +0100

    update: Add labeling script for smely-zajko data (#14)
    * Added posibility to label path
        - white is a class for path
    * Added posibility to label horizont
        - blue is a class for horizont
    * Added loader for .trn files and .xml files
    * Mask is now saved as <filename>_mask.png to same directory that the
    original image was loaded from
    
    Signed-off-by: Ondrej <o.jariabka@gmail.com>

---
+++ labeler.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+import argparse
+import os.path as os
+import cv2 as cv
+import numpy as np
+
+
+horz = 0
+mask = None
+color = None
+flood = False
+rec_size = 5
+line_points = np.array([[-1, -1], [-1, -1]])
+
+
+def load_xml(filename):
+    return np.asarray(cv.cv.Load(filename)) * 255
+
+
+def load_trn(filename):
+    filename_img = filename.replace('.trn', '')
+    img = cv.imread(filename_img)
+    with open(filename, 'r') as f:
+        file = f.read().rstrip()
+        items = map(int, file.split(' '))
+        numpy_array = np.asarray(items, np.uint8) * 255
+
+        return numpy_array.reshape(img.shape[0], img.shape[1])
+
+
+def floodify(img, color, x, y):
+    flags = 4
+    flags | 255 << 8
+    h, w = img.shape[:2]
+    mask = np.zeros((h + 2, w + 2), np.uint8)
+    # get only path part of mask (white parts)
+    tmp_img = (img == [255, 255, 255]).astype('uint8') * 255
+    cv.floodFill(tmp_img, mask, (x, y), color, 0, 255, flags)
+    return tmp_img
+
+
+def get_line_coords(img):
+    global line_points
+    width = img.shape[1]
+    x_1, y_1 = line_points[0]
+    x_c, y_c = line_points[1] - line_points[0]
+    x_n1 = -x_1 / x_c
+    x_n2 = (width - x_1) / x_c
+    y_new1 = y_1 + x_n1 * y_c
+    y_new2 = y_1 + x_n2 * y_c
+    return (0, y_new1), (width, y_new2)
+
+
+# mouse callback function
+def draw(event, x, y, flags, param):
+    global mask, horz, color, flood, rec_size, line_points
+    if event == cv.EVENT_LBUTTONDOWN:
+        color = (255, 255, 255)
+        if horz != 0:
+            line_points[horz - 1] = [x, y]
+        elif flood is True:
+            mask[np.nonzero(floodify(mask, color, x, y))] = 255
+
+    if event == cv.EVENT_RBUTTONDOWN:
+        color = (0, 0, 0)
+        if flood is True:
+            floodify(mask, color, x, y)
+
+    if event == cv.EVENT_MOUSEMOVE and color is not None:
+        cv.rectangle(mask, (x - rec_size, y - rec_size),
+                           (x + rec_size, y + rec_size), color, -1)
+
+    if event == cv.EVENT_LBUTTONUP or event == cv.EVENT_RBUTTONUP:
+        if horz == 2:
+            p1, p2 = get_line_coords(mask)
+            pts = np.array([p1, p2, (mask.shape[1], 0), (0, 0)])
+            cv.fillPoly(mask, [pts], (255, 0, 0))
+            horz %= 2
+        flood = False
+        color = None
+
+
+def main_event_loop(image):
+    global flood, rec_size, horz, mask
+    while(1):
+        img = cv.addWeighted(image, 0.7, mask.astype('uint8'), 0.3, 0)
+        cv.imshow("labeling board", img)
+        cv.imshow("mask", mask)
+        k = cv.waitKey(1) & 0xFF
+        # floodfill keybind
+        if k == ord('f'):
+            flood = True
+        # add new point to horizont keybind
+        if k == ord('h'):
+            horz += 1
+        # enlarge the brush
+        if k == ord('+'):
+            rec_size += 1
+            print(rec_size)
+        # reduce the brush
+        if k == ord('-'):
+            rec_size -= 1
+            print(rec_size)
+        # end & save keybinds
+        if k == 27 or k == ord('q'):
+            break
+
+
+def load_mask(mask_name):
+    if os.splitext(mask_name)[1] == '.trn':
+        mask = load_trn(mask_name)
+        mask = np.repeat(mask, 3).reshape((mask.shape[0],
+                                           mask.shape[1], 3))
+    elif os.splitext(mask_name)[1] == '.xml':
+        mask = load_xml(mask_name)
+        mask = np.repeat(mask, 3).reshape((mask.shape[0],
+                                           mask.shape[1], 3))
+    else:
+        mask = cv.imread(mask_name, cv.CV_LOAD_IMAGE_COLOR)
+        mask = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
+    return mask
+
+
+def main(image, prep_mask=None):
+    global mask
+    im = cv.imread(image, cv.CV_LOAD_IMAGE_COLOR)
+    if prep_mask is not None:
+        mask = load_mask(prep_mask)
+    else:
+        mask = np.zeros(im.shape)
+    # create labeling board
+    cv.namedWindow('labeling board')
+    cv.setMouseCallback('labeling board', draw)
+    main_event_loop(im)
+    cv.destroyAllWindows()
+    # save the created mask
+    cv.imwrite("{}_mask.png".format(os.splitext(image)[0]), mask)
+
+
+if __name__ == '__main__':
+    desc = 'Smely-Zajko labeling tool'
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('filename', metavar='filename',
+                        help='Image to label.')
+    parser.add_argument('-m', '--mask', nargs='?', metavar='filename',
+                        help='Predifined mask.', dest='mask')
+    args = parser.parse_args()
+    main(args.filename, args.mask)
